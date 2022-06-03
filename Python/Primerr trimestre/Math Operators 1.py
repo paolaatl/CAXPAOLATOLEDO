0a2f2434a3d3dab7da6556510d0d214413541c81
paolaatl <104523364+paolaatl@users.noreply.github.com>
2022-06-03 18:32:23 -0500

    Add files via upload

---
+++ Python/Primerr trimestre/Math Operators 1.py
@@ -0,0 +1,50 @@
+esp1 = 8
+esp2 = 10
+esp3 = 9
+
+sumaydivesp= esp1 + esp2 + esp3/3
+print('el promedio de espanol es', sumaydivesp)
+
+mct1 = 10
+mct2 = 8
+mct3 = 6
+
+sumaydivmct = mct1 + mct2 + mct3/3
+print('el promedio de mct es:',sumaydivmct)
+
+PE1 = 10
+PE2 = 10
+PE3 = 6
+
+sumaPE = PE1 + PE2 + PE3/3
+print ('el promedio de PE es:', sumaPE)
+
+math1 = 8
+math2 = 10
+math3 = 9
+
+sumath= math1 + math2 + math3/3
+print ('el promedio de matematicas es:', sumath)
+
+LA1 = 9
+LA2 = 8
+LA3 = 9
+
+sumaLA = LA1 + LA2 + LA3/3
+print('el promedio de LA es:', sumaLA)
+
+ESPANOL = 21.0
+MCT = 20.0
+PE = 22.0
+MATEMATICAS = 21.0
+LA = 20.0
+
+sumallmaterias = ESPANOL + MCT + PE + MATEMATICAS + LA
+
+print('la suma de todas las materias ea:', sumallmaterias)
+
+allmaterias = 104.0
+print ('mi promedio es de:', allmaterias)
+
+
+#no se si lo hice bien 🙂🔫
