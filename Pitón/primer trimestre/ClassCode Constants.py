6d6d344516a47d99896bb10d408dc0e6ecf85fe6
paolaatl <104523364+paolaatl@users.noreply.github.com>
2022-06-03 18:17:20 -0500

    Add files via upload

---
+++ Pitón/primer trimestre/ClassCode Constants.py
@@ -0,0 +1,14 @@
+print('aprobados')
+print(5)
+
+mensaje='aprobados'
+mensaje2='reprobados'
+
+print(mensaje)
+print(mensaje2)
+
+mensaje='menos tarea para el 9 virtual'
+
+print(mensaje)
+
+print('este es el mensaje guardado en memoria:', mensaje)
